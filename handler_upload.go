a4989eadde599be2dcf825115f7ff159b39fda88
Stoney Kang <sikang99@gmail.com>
2019-10-15 12:37:17 +0900

    event handling using SSE

--- handler_upload.go
+++ handler_upload.go
@@ -9,28 +9,27 @@ import (
 
 func uploadHandler(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	log.Printf(r.URL.Path)
-	// Parse our multipart form, 10 << 20 specifies a maximum
-	// upload of 10 MB files.
+	// Parse our multipart form, 10 << 20 specifies a maximum upload of 10 MB files.
 	r.ParseMultipartForm(10 << 20)
 	// FormFile returns the first file for the given key `myFile`
 	// it also returns the FileHeader so we can get the Filename,
 	// the Header and the size of the file
 	file, handler, err := r.FormFile("myFile")
 	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
+		log.Printf("FormFile error: %s", err)
 		return
 	}
 	defer file.Close()
+
 	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
 	fmt.Printf("File Size: %+v\n", handler.Size)
 	fmt.Printf("MIME Header: %+v\n", handler.Header)
 
-	// Create a temporary file within our temp-images directory that follows
-	// a particular naming pattern
+	// Create a temp file within our upload directory that follows a particular naming pattern
 	tempFile, err := ioutil.TempFile("upload", "upload-*.png")
 	if err != nil {
-		fmt.Println(err)
+		log.Printf("TempFile error: %s", err)
+		return
 	}
 	defer tempFile.Close()
 
@@ -38,7 +37,7 @@ func uploadHandler(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	// byte array
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
-		fmt.Println(err)
+		log.Println("ReadAll error: %s", err)
 	}
 	// write this byte array to our temporary file
 	tempFile.Write(fileBytes)
