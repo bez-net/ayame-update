cf881eb0b801e226cfb948efce36df12d7fc0458
Stoney Kang <sikang99@gmail.com>
2019-10-24 20:13:54 +0900

    19.04.08: start chat support

--- handler_upload.go
+++ handler_upload.go
@@ -23,9 +23,9 @@ func uploadHandler(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	}
 	defer file.Close()
 
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
+	// fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+	// fmt.Printf("File Size: %+v\n", handler.Size)
+	// fmt.Printf("MIME Header: %+v\n", handler.Header)
 
 	// Create a temp file within our upload directory that follows a particular naming pattern
 	tempFile, err := ioutil.TempFile("upload", "cojam-*"+filepath.Ext(handler.Filename))
@@ -35,8 +35,7 @@ func uploadHandler(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	}
 	defer tempFile.Close()
 
-	// read all of the contents of our uploaded file into a
-	// byte array
+	// read all of the contents of our uploaded file into a byte array
 	fileBytes, err := ioutil.ReadAll(file)
 	if err != nil {
 		log.Println("ReadAll error: %s", err)
@@ -45,6 +44,7 @@ func uploadHandler(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	tempFile.Write(fileBytes)
 	// return that we have successfully uploaded our file!
 	fmt.Fprintf(w, "Successfully uploaded file\n")
+	log.Printf("%s is stored to %s", handler.Filename, tempFile.Name())
 }
 
 // Send a web page to the http client
