842d13df59d6b8e7021ca316677454a4f8b8046b
Stoney Kang <sikang99@gmail.com>
2019-12-04 13:57:38 +0900

    19.05.04: upload files using fetch

--- handler_signal.go
+++ handler_signal.go
@@ -171,8 +171,9 @@ func signalHandler(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s, %s", r.URL.Path, r.RemoteAddr)
 	defer log.Printf("signalHandler exit")
 
-	// allow all CORS
+	// allow all CORS for normal http
 	// w.Header().Set("Access-Control-Allow-Origin", "*")
+	// w.Header().Set("Access-Control-Allow-Credentials", "true")
 	// w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	// w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
